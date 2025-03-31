768661085b4a161685f10f769786e274e2c9bf8f
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-03-31 20:50:02 -0300

    Delete Main.java

--- ProdutoFornecedor/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Fornecedor fornecedor = new Fornecedor("aa", 0);
-        System.out.println(fornecedor.getNome());
-        fornecedor.setNome("bb");
-        System.out.println(fornecedor.getNome());
-    }
-}
