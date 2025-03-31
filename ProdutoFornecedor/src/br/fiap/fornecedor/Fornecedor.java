67b5ebea798fb1609547e20b6049d1d84ca23ac9
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-03-31 20:49:42 -0300

    Create Fornecedor.java

---
+++ ProdutoFornecedor/src/br/fiap/fornecedor/Fornecedor.java
@@ -0,0 +1,28 @@
+package br.fiap.fornecedor;
+
+public class Fornecedor {
+    private String nome;
+    private long cnpj;
+
+    public Fornecedor(String nome, long cnpj) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCnpj() {
+        return this.cnpj;
+    }
+
+    public void setCnpj(long cnpj) {
+        this.cnpj = cnpj;
+    }
+
+}
